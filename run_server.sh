964f6c7902324c65717f084fd8bde7cb7ab1989f
farheen <farheenfaisal.ak@gmail.com>
2025-05-21 22:30:01 +0400

    Updated and fixed errors in .sh file for running server

--- run_server.sh
+++ run_server.sh
@@ -5,13 +5,16 @@ echo "Starting VisionPlay Hub..."
 echo "============================"
 
 # Step 1: Activate virtual environment and run Django migrations
+# python3 -m venv venv   
+source venv/bin/activate
 cd backend 
-source ../venv/bin/activate
-python manage.py makemigrations
-python manage.py migrate
+#pip install django 
+python3 manage.py makemigrations
+python3 manage.py migrate
 
 # Step 2: Build the React frontend
-cd ../frontend 
+cd ../frontend
+#npm install 
 npm run build
 
 # Step 3: Copy the compiled main.*.js to backend static folder
@@ -21,8 +24,8 @@ cp -f "$main_js" ../backend/static/js/app.js
 
 # Step 4: Collect Django static files
 cd ../backend || exit
-python manage.py collectstatic --noinput
+python3 manage.py collectstatic --noinput
 
 # Step 5: Launch server and browser
 open http://127.0.0.1:8000
-python manage.py runserver
+python3 manage.py runserver
