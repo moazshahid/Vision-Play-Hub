3052b55722fa64cd0a8915f317ca08683fa73a4d
Aahana Bhowmick <aahanabhowmick@gmail.com>
2025-06-01 12:44:53 +0400

    feat(game): created DessertSlash.js main game logic file

---
+++ frontend/src/DessertSlash.js
@@ -0,0 +1,16 @@
+import React, { useEffect, useRef } from 'react';
+import './Game.css';
+
+const DessertSlash = () => {
+  useEffect(() => {
+    // setup will go here
+  }, []);
+
+  return (
+    <div>
+      <p>Dessert Slash Game (coming soon)</p>
+    </div>
+  );
+};
+
+export default DessertSlash;
